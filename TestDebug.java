99f6d4634a7f8d56918481d1aee0881c67db6d4a
方斩仙 <33818543+hemwick@users.noreply.github.com>
2019-02-28 15:36:49 +0800

    Add files via upload

---
+++ TestDebug.java
@@ -0,0 +1,25 @@
+package day02;
+
+public class TestDebug {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		
+		int j=100;
+		
+		i=i-j;
+		
+		System.out.println(i);
+		
+		getPrize();
+		
+		System.out.println("333333333333");
+	}
+	public static int getPrize() {
+		System.out.println("111111111111");
+		System.out.println("222222222222");
+		return 100;
+	}
+
+}
